05e29a84a362a395e4cb99b8dd8304c9ea9ea6be
xifOO <wedyi28111@gmail.com>
2026-04-13 17:38:47 +0300

    feat: add register required stats decorator

--- pills_core/strategies/util.py
+++ pills_core/strategies/util.py
@@ -1,6 +1,5 @@
 from typing import Callable, TypeVar
 
-
 ClsT = TypeVar("ClsT", bound=type)
 
 
@@ -9,13 +8,15 @@ def register_required_stats(*stats: str) -> Callable[[ClsT], ClsT]:
     Decorator for register required stats for strategy.
     Example:
         @register_required_stats("median", "mean")
-        class MedianImputation: 
+        class MedianImputation:
     """
+
     def decorator(cls: ClsT) -> ClsT:
         parent_stats = set()
         for base in cls.__mro__:
             if hasattr(base, "required_stats"):
-                parent_stats.update(getattr(base, "required_stats"))
+                parent_stats.update(base.required_stats)
         cls.required_stats = parent_stats.union(stats)
         return cls
+
     return decorator
